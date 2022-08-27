42e1a736e896dc7962ad91bfc5e7b0e634517949
ErikaCarvajalQ <mirollo19@hotmail.com>
2022-08-26 22:52:42 -0500

    Proyecto Erika Carvajal

---
+++ OrgSolution_Neterik/src/test/java/com/Desarrolladores11/OrgSolution_Net/OrgSolutionNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Desarrolladores11.OrgSolution_Net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgSolutionNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
